74dc073ca5ba004a34716648d43ce73a8fe4cb9f
fllcker <96314993+jxwide@users.noreply.github.com>
2023-01-12 17:49:37 +0300

    init

---
+++ src/test/java/ru/fllcker/urlshorter/UrlshorterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fllcker.urlshorter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlshorterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
